da6ad694876a59ddea8d32d12ad677e4db30791d
rob <robdelacruz@gmail.com>
2024-02-15 20:22:22 +0800

    expenses listbox

--- tui.c
+++ tui.c
@@ -4,6 +4,9 @@
 #include "clib.h"
 #include "db.h"
 
+#define COLOR_FG TB_GREEN
+#define COLOR_BG TB_BLACK
+
 typedef struct {
     uint x;
     uint y;
@@ -11,31 +14,50 @@ typedef struct {
     uint height;
 } rect_t;
 
-typedef struct {
+typedef struct listbox_s {
     rect_t rect;
     int sel;
     int scrollpos;
+    array_t *rows;
+    void (*draw_row)(struct listbox_s *lb, int irow, uintattr_t fg, uintattr_t bg);
 } listbox_t;
 
-static void update_listbox(listbox_t *lb, array_t *xps, struct tb_event *ev);
-static void draw_listbox(listbox_t *lb, array_t *xps);
-static void draw_listbox_row(listbox_t *lb, char *s, int rownum, uintattr_t fg, uintattr_t bg);
+typedef void (listbox_draw_row_fn)(listbox_t *lb, int irow, uintattr_t fg, uintattr_t bg);
+
+static void listbox_update(listbox_t *lb, struct tb_event *ev);
+static void listbox_draw(listbox_t *lb);
+static void listbox_draw_row(listbox_t *lb, int irow, uintattr_t fg, uintattr_t bg);
+static void listbox_draw_string_row(listbox_t *lb, char *s, int irow, uintattr_t fg, uintattr_t bg);
+static void listbox_draw_xp_row(listbox_t *lb, int irow, uintattr_t fg, uintattr_t bg);
+
+static listbox_t *listbox_new(uint x, uint y, uint width, uint height, array_t *rows, listbox_draw_row_fn *draw_row) {
+    listbox_t *lb = malloc(sizeof(listbox_t));
+    lb->rect = (rect_t) {x, y, width, height};
+    lb->sel = 0;
+    lb->scrollpos = 0;
+    lb->rows = rows;
+    if (draw_row != NULL)
+        lb->draw_row = draw_row;
+    else
+        lb->draw_row = listbox_draw_row;
+    return lb;
+}
 
 void tui_start(sqlite3 *db, char *dbfile) {
     struct tb_event ev;
+    listbox_t *lb;
+    array_t *xps = array_new(0);
 
     tb_init();
     tb_set_input_mode(TB_INPUT_ALT);
-    tb_set_clear_attrs(TB_WHITE, TB_BLUE);
+    tb_set_clear_attrs(COLOR_FG, COLOR_BG);
 
-    rect_t lbrect = {1,0, tb_width()-2, tb_height()-1};
-    listbox_t lb = {lbrect, 0, 0};
-    array_t *xps = array_new(0);
     db_select_exp(db, xps);
+    lb = listbox_new(0,0, tb_width(), tb_height()-1, xps, listbox_draw_xp_row);
 
     tb_clear();
-    tb_printf(0, tb_height()-1, TB_DEFAULT,TB_DEFAULT, "sel: %d, scrollpos: %d", lb.sel, lb.scrollpos);
-    draw_listbox(&lb, xps);
+    tb_printf(0, tb_height()-1, TB_DEFAULT,TB_DEFAULT, "sel: %d, scrollpos: %d", lb->sel, lb->scrollpos);
+    listbox_draw(lb);
     tb_present();
 
     while (1) {
@@ -46,16 +68,16 @@ void tui_start(sqlite3 *db, char *dbfile) {
             break;
 
         tb_clear();
-        tb_printf(0, tb_height()-1, TB_DEFAULT,TB_DEFAULT, "sel: %d, scrollpos: %d", lb.sel, lb.scrollpos);
-        update_listbox(&lb, xps, &ev);
-        draw_listbox(&lb, xps);
+        tb_printf(0, tb_height()-1, TB_DEFAULT,TB_DEFAULT, "sel: %d, scrollpos: %d", lb->sel, lb->scrollpos);
+        listbox_update(lb, &ev);
+        listbox_draw(lb);
         tb_present();
     }
 
     tb_shutdown();
 }
 
-static void update_listbox(listbox_t *lb, array_t *xps, struct tb_event *ev) {
+static void listbox_update(listbox_t *lb, struct tb_event *ev) {
     if (ev->key == TB_KEY_ARROW_UP || ev->ch == 'k')
         lb->sel--;
     if (ev->key == TB_KEY_ARROW_DOWN || ev->ch == 'j')
@@ -67,53 +89,73 @@ static void update_listbox(listbox_t *lb, array_t *xps, struct tb_event *ev) {
 
     if (lb->sel < 0)
         lb->sel = 0;
-    if (lb->sel > xps->len-1)
-        lb->sel = xps->len-1;
+    if (lb->sel > lb->rows->len-1)
+        lb->sel = lb->rows->len-1;
 
     if (lb->sel < lb->scrollpos)
         lb->scrollpos = lb->sel;
     if (lb->sel > lb->scrollpos + lb->rect.height-1)
         lb->scrollpos = lb->sel - (lb->rect.height-1);
 
-    if (lb->scrollpos > xps->len-1)
-        lb->scrollpos = xps->len-1;
+    if (lb->scrollpos > lb->rows->len-1)
+        lb->scrollpos = lb->rows->len-1;
 }
 
-static void draw_listbox(listbox_t *lb, array_t *xps) {
-    int rownum = 0;
+static void listbox_draw_xp_row(listbox_t *lb, int irow, uintattr_t fg, uintattr_t bg) {
+    char buf[4096];
+    char isodate[ISO_DATE_LEN+1];
+    exp_t *xp = lb->rows->items[irow];
+
+    date_to_iso(xp->date, isodate, sizeof(isodate));
+    snprintf(buf, sizeof(buf), "%s %s %.2f %s", isodate, xp->desc->s, xp->amt, xp->catname->s);
+    listbox_draw_string_row(lb, buf, irow, fg, bg);
+}
+
+static void listbox_draw(listbox_t *lb) {
+    int irow = 0;
     exp_t *xp;
+    listbox_draw_row_fn *draw_row;
 
-    if (lb->sel > xps->len-1)
-        lb->sel = xps->len-1;
-    if (lb->scrollpos > xps->len-1)
-        lb->scrollpos = xps->len-1;
+    if (lb->sel > lb->rows->len-1)
+        lb->sel = lb->rows->len-1;
+    if (lb->scrollpos > lb->rows->len-1)
+        lb->scrollpos = lb->rows->len-1;
 
-    for (int i = lb->scrollpos; i < xps->len; i++) {
-        if (rownum > lb->rect.height-1)
+    if (lb->draw_row != NULL)
+        draw_row = lb->draw_row;
+    else
+        draw_row = listbox_draw_row;
+
+    for (int i = lb->scrollpos; i < lb->rows->len; i++) {
+        if (irow > lb->rect.height-1)
             break;
 
-        xp = xps->items[i];
         if (i == lb->sel)
-            draw_listbox_row(lb, xp->desc->s, rownum, TB_BLUE, TB_WHITE);
+            (*draw_row)(lb, irow, COLOR_BG, COLOR_FG);
         else
-            draw_listbox_row(lb, xp->desc->s, rownum, TB_WHITE, TB_BLUE);
-        rownum++;
+            (*draw_row)(lb, irow, COLOR_FG, COLOR_BG);
+        irow++;
     }
 }
 
-static void draw_listbox_row(listbox_t *lb, char *s, int rownum, uintattr_t fg, uintattr_t bg) {
+static void listbox_draw_row(listbox_t *lb, int irow, uintattr_t fg, uintattr_t bg) {
+    char *s = lb->rows->items[irow];
+    listbox_draw_string_row(lb, s, irow, fg, bg);
+}
+
+static void listbox_draw_string_row(listbox_t *lb, char *s, int irow, uintattr_t fg, uintattr_t bg) {
     size_t s_len = strlen(s);
     int i=0;
     int is = 0;
 
     assert(lb->rect.width > 0);
-    assert(rownum >= 0 && rownum < lb->rect.height);
+    assert(irow >= 0 && irow < lb->rect.height);
 
-    tb_set_cell(lb->rect.x+i, lb->rect.y+rownum, ' ', fg, bg);
+    tb_set_cell(lb->rect.x+i, lb->rect.y+irow, ' ', fg, bg);
     i++;
 
     while (i < lb->rect.width && is < s_len) {
-        tb_set_cell(lb->rect.x+i, lb->rect.y+rownum, s[is], fg, bg);
+        tb_set_cell(lb->rect.x+i, lb->rect.y+irow, s[is], fg, bg);
         i++;
         is++;
     }
@@ -123,7 +165,7 @@ static void draw_listbox_row(listbox_t *lb, char *s, int rownum, uintattr_t fg, 
     assert(is == s_len);
 
     while (i < lb->rect.width) {
-        tb_set_cell(lb->rect.x+i, lb->rect.y+rownum, ' ', fg, bg);
+        tb_set_cell(lb->rect.x+i, lb->rect.y+irow, ' ', fg, bg);
         i++;
     }
 }
